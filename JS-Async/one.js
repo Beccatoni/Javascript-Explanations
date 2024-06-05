b478edc7a80bcfa668715d2c0f20e0dbb5d88c9c
Beccatoni <rbccmtni1@gmail.com>
2024-06-05 20:30:04 +0200

    new displaying the settimeout intervalid

--- JS-Async/one.js
+++ JS-Async/one.js
@@ -5,3 +5,8 @@ function greet(){
 
 setTimeout(greet, 3000);
 console.log('This message is shown first');
+
+// as the setTimeout returns the interval id
+
+let intervalId = setTimeout(greet, 3000);
+console.log('Id:', intervalId);
