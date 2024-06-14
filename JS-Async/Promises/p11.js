d8b7876720aa6803b07ae105de12c8e2d1a78003
Beccatoni <rbccmtni1@gmail.com>
2024-06-14 17:04:30 +0200

    promises creating a simple alarm using promises fetching data from the different apis at the same time

---
+++ JS-Async/Promises/p11.js
@@ -0,0 +1,18 @@
+function createAlarm(name, delay){
+    return new Promise((resolve, reject)=>{
+      if(delay < 2){
+        reject(' Delay is not sufficient');
+      }else{
+        setTimeout(()=> {
+          resolve(`Wake up ${name}`);
+        }, delay * 1000);
+      }
+    })
+}
+  
+  createAlarm('John', 4)
+  .then((message)=>{
+    console.log(message)
+  }).catch((error)=>{
+    console.log(error);
+  });
